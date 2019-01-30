a78dd126392e6357ccedddb1a8551d913269a2a2
kvsemenoff <kvsemenoff@mail.ru>
2019-01-30 17:31:54 +0300

    натяжка и доработки

--- wp-content/themes/ikos/404.php
+++ wp-content/themes/ikos/404.php
@@ -1,24 +1,17 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
+	
 
-			<!-- article -->
-			<article id="post-404">
 
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
+	<div class="menuList container">
+		<h1 class="h1"><?php _e( '404. Page not found', 'html5blank' ); ?></h1>
+		<h2>
+			<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
+		</h2>
+	</div>
 
-			</article>
-			<!-- /article -->
+	</div>
 
-		</section>
-		<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
+
